4e542b7a24b9050bbf95f2b3e520ff6070f8e610
IridescentNull <mitome19@gmail.com>
2017-10-20 14:25:45 +0900

    test version1.5

--- LotusPetal.java
+++ LotusPetal.java
@@ -1,4 +1,6 @@
 public class LotusPetal{
 	public statis void main(String[] args){
 		System.out.println("Which color do you like?");
+
+		System.out.println("The ultimate Gamemaster can defeat the ultimate Creater?")
 }}
