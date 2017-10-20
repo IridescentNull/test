7ee034473bc69a4cd91a13cdff8833d55e76b988
IridescentNull <mitome19@gmail.com>
2017-10-20 17:38:35 +0900

    test version1.9

--- LotusPetal.java
+++ LotusPetal.java
@@ -2,9 +2,21 @@ public class LotusPetal{
 	public static void main(String[] args){
 		System.out.println("Which color do you like?");
 
-		System.out.println("The ultimate Gamemaster can defeat the ultimate Creater?");
+		System.out.println("The ultimate Gamemaster can defeat the ultimate Creator?");
 
 		System.out.println("Does the ultimate solo-player have dreams with the electronic zombie?");
 
 		System.out.println("Hmm... I have a question about you, Ms Miu. ---What do you eat this morning?");
+
+		System.out.println("Yes, I didn't eat anything either. ---Ms Miu, how about the meal previous that?") ;
+
+		System.out.println("Yes, I didn't have that, ...and NEVER do in this academy!");
+
+		System.out.println("Stop, stop Saihara! I got it, I know, ...this fucking school has many many strange points!");
+
+		System.out.println("But, why, ...why do you interrogate me? I don't..." );
+
+		System.out.println("I realize that, Miu, YOU are the ringleader of this corpse party!");
+		
+		
 }}
